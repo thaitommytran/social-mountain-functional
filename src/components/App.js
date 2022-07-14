287679b003c5be9c7b518d99e896db8ca119eb4b
Thai Tran <thai.tommy.tran@gmail.com>
2022-07-14 00:57:51 -0500

    step 1 completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
-import Post from './Post/Post'
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 const App = () => {
-
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    
-  }, [])
+    axios
+      .get("https://practiceapi.devmountain.com/api/posts")
+      .then((res) => setPosts(res.data));
+  }, []);
 
+  const updatePost = () => {};
 
-  const updatePost = () => {
-    
-  }
+  const deletePost = () => {};
 
-  const deletePost = () => {
-    
-  }
-
-  const createPost = () => {
-    
-  }
+  const createPost = () => {};
 
   return (
-    <div className='App__parent'>
+    <div className="App__parent">
       <Header />
 
-      <section className='App__content'>
-        <Compose  />
+      <section className="App__content">
+        <Compose />
+        {posts.map((post) => {
+          return (
+            <Post
+              key={post.id}
+              text={post.text}
+              date={post.date}
+              deletePostFn={deletePost}
+            />
+          );
+        })}
       </section>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,73 +1,75 @@
-import React, { useState } from 'react';
-import ProfileIcon from 'react-icons/lib/md/person-outline';
-import ReplyIcon from 'react-icons/lib/md/chat-bubble-outline';
-import FavoriteIcon from 'react-icons/lib/md/favorite-outline';
-import MessageIcon from 'react-icons/lib/md/mail-outline';
-import MasterControlIcon from 'react-icons/lib/md/more-vert';
+import React, { useState } from "react";
+import ProfileIcon from "react-icons/lib/md/person-outline";
+import ReplyIcon from "react-icons/lib/md/chat-bubble-outline";
+import FavoriteIcon from "react-icons/lib/md/favorite-outline";
+import MessageIcon from "react-icons/lib/md/mail-outline";
+import MasterControlIcon from "react-icons/lib/md/more-vert";
 
-import './Post.css';
+import "./Post.css";
 
-import Edit from './Edit/Edit';
+import Edit from "./Edit/Edit";
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
 
 const Post = (props) => {
-
-  const [editing, setEditing] = useState(false)
-  const [showMasterMenu, setShowMasterMenu] = useState(false)
+  const { text, date } = props;
+  const [editing, setEditing] = useState(false);
+  const [showMasterMenu, setShowMasterMenu] = useState(false);
 
   // This puts the post into EDIT mode when the EDIT button is clicked from the drop-down
   const showEdit = () => {
-    setEditing(true)
-    setShowMasterMenu(false)
-  }
+    setEditing(true);
+    setShowMasterMenu(false);
+  };
 
   // This puts the post back into normal viewing mode when the CANCEL button is clicked
   // This method is passed down to the <Edit /> component via props
-  const hideEdit= () => {
-    setEditing(false)
-  }
+  const hideEdit = () => {
+    setEditing(false);
+  };
 
   // This toggles the drop-down when the three dots in the top right corner of a post are clicked
   const toggleMasterMenu = () => {
-    setShowMasterMenu(!showMasterMenu)
-  }
+    setShowMasterMenu(!showMasterMenu);
+  };
 
   // This hides the drop-down when the post is clicked anywhere
   const hideMasterMenu = () => {
-    if(showMasterMenu){
-      setShowMasterMenu(false)
+    if (showMasterMenu) {
+      setShowMasterMenu(false);
     }
-  }
+  };
 
   return (
-      // Main body of post
-      <section className="Post__parent" onClick={ hideMasterMenu }>
+    // Main body of post
+    <section className="Post__parent" onClick={hideMasterMenu}>
+      {/* Three dots in top right corner */}
+      <div className="Post__master-controls">
+        <MasterControlIcon onClick={toggleMasterMenu} />
 
-        {/* Three dots in top right corner */}
-        <div className="Post__master-controls">
-          <MasterControlIcon onClick={ toggleMasterMenu } />
+        {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
+        <div
+          className="Post__master-menu"
+          style={{ display: showMasterMenu ? "flex" : "none" }}
+        >
+          <span onClick={showEdit}>Edit</span>
+          <span>Delete</span>
+        </div>
+      </div>
 
-          {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
-          <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
-            <span onClick={ showEdit }>Edit</span>
-            <span>Delete</span>
-          </div>
+      {/* This is where all the meta data of the post will go (who, when, where) */}
+      <div className="Post__meta-data">
+        <div className="Post__profile-picture">
+          <ProfileIcon />
         </div>
 
-        {/* This is where all the meta data of the post will go (who, when, where) */}
-        <div className="Post__meta-data">
-          <div className="Post__profile-picture">
-            <ProfileIcon />
-          </div>
+        <span className="Post__name">DevMountain</span>
+        <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__name">DevMountain</span>
-          <span className="Post__handle">@DevMountain</span>
+        <span className="Post__date">Date Here</span>
+      </div>
 
-          <span className="Post__date">Date Here</span>
-        </div>
-
-        {/* This is where the text goes. Notice the ternary statement. The ternary statement decides to display either the text OR the editor view
+      {/* This is where the text goes. Notice the ternary statement. The ternary statement decides to display either the text OR the editor view
             You can also think of it as being written as so:
               if( editing === true ) {
                 <Edit ... />
@@ -75,28 +77,25 @@ const Post = (props) => {
                 <span ... ></span>
               }
         */}
-        <div className="Post__content">
-          {
-            // This has been pulled off of this.state via destructuring
-            editing
-            ?
-              <Edit text={ text }
-                    hideEdit={ hideEdit }
-                    />
-            :
-              <span className="Post__text">{ text }</span>
-          }
-        </div>
+      <div className="Post__content">
+        {
+          // This has been pulled off of this.state via destructuring
+          editing ? (
+            <Edit text={text} hideEdit={hideEdit} />
+          ) : (
+            <span className="Post__text">{text}</span>
+          )
+        }
+      </div>
 
-        {/* These are all of the cute little icons in the bottom left corner */}
-        <div className="Post__user-controls">
-          <ReplyIcon className="Post__control-icon" />
-          <FavoriteIcon className="Post__control-icon" />
-          <MessageIcon className="Post__control-icon" />
-        </div>
+      {/* These are all of the cute little icons in the bottom left corner */}
+      <div className="Post__user-controls">
+        <ReplyIcon className="Post__control-icon" />
+        <FavoriteIcon className="Post__control-icon" />
+        <MessageIcon className="Post__control-icon" />
+      </div>
+    </section>
+  );
+};
 
-      </section>
-    )
-  }
-
-export default Post
+export default Post;
